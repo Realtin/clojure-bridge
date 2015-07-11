2edb694b5533dd104e6274470c59bdf0e0c5e0b8
realtin <juliakbln@gmail.com>
2015-07-11 17:35:10 +0200

    now with comments

--- src/drawing/core.clj
+++ src/drawing/core.clj
@@ -10,30 +10,27 @@
 (def height (q/screen-height))
 
 (defn abs [z]
+  ; get the absolute value of a number
   (if (neg? z)
     (- z)
     z))
 
 (defn set-color [mx my]
+  ; set color according to the mouse position
+  ; lines
   (q/stroke (abs (- mx 255))
             (abs (- my 255))
             (abs (/ (+ mx my) 2)))
+  ; rectangle
   (q/fill (abs (- mx 255))
             (abs (- my 255))
             (abs (/ (+ mx my) 2))))
-
-;(defn rectangle [edge xpos ypos]
-;  (cond 
-;    (= edge 00) [(- xpos 10) ypos]
-;    (= edge 01) [(+ xpos 10) ypos]
-;    (= edge 10) [xpos (- ypos 10)]
-;    (= edge 11) [xpos (+ ypos 10)]
-;    :else [0 0]))
   
 (defn size-rect [x y size]
    (q/rect (- x (/ size 2)) (- y (/ size 2)) size size ))
 
 (defn perspective-rec []
+  ; rectangle connected to the corners of the screen
   (let [x (q/mouse-x)
         y (q/mouse-y)
         size (/ y 2)]
@@ -41,6 +38,7 @@
   (q/line width 0 (+ x (/ size 2)) (- y (/ size 2)))
   (q/line 0 height (- x (/ size 2)) (+ y (/ size 2)))
   (q/line width height (+ x (/ size 2)) (+ y (/ size 2)))
+  ; rectangle that changes its size according to the mouse position
   (size-rect x y size)))
 
 (defn draw []
@@ -51,7 +49,7 @@
 
 
 (q/defsketch hello-lines
-  :title "Rainbow-lines"
+  :title "Responsive-Rainbow-Rectangle"
   :size :fullscreen
   :features [:present]
   :setup setup
